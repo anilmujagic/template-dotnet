e8f9ad09d02fc34862e32d24159c8e429139a82b
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 04:23:40 +0100

    Add the build script

---
+++ build/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Prevent packing of resource fork files on macOS (./._xxxxx)
+export COPYFILE_DISABLE=true
+
+set -e
+
+pushd "${0%/*}" # Go to script directory
+build_dir="$(pwd)"
+temp_dir="$build_dir/temp"
+output_dir="$build_dir/output"
+
+rm -rf "$temp_dir"
+mkdir -p -m 777 "$temp_dir"
+
+rm -rf "$output_dir"
+mkdir -p "$output_dir"
+
+################################################################################
+# Application
+################################################################################
+project_names="
+    MyApp.Api
+"
+
+for project_name in $project_names; do
+    echo "--- Building project: $project_name"
+
+    project_source_dir="../src/$project_name"
+    project_temp_dir="$temp_dir/$project_name"
+
+    pushd "$project_source_dir"
+    mkdir -p "$project_temp_dir"
+    dotnet publish -c Release -o "$project_temp_dir"
+    popd
+
+    pushd "$project_temp_dir"
+    mv appsettings.json appsettings.json.local
+    tar czf "$output_dir/$project_name.tar.gz" --exclude={appsettings.json,*.pdb} *
+    popd
+done
+
+################################################################################
+# Database
+################################################################################
+pushd "../db"
+tar czf "$output_dir/db.tar.gz" *
+popd
+
+################################################################################
+# Finalize
+################################################################################
+rm -rf "$temp_dir" # Clean up
+popd # Go back to caller directory
+
+# Show build output
+echo "--- Build output in $output_dir"
+ls -lh "$output_dir"
