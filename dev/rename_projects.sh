99ad5c2339483d4d7df3595d3857334a04b48d0c
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 14:18:44 +0100

    Add project rename script

---
+++ dev/rename_projects.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+pushd "${0%/*}" # Go to script directory
+
+cd ..
+
+#old_name=$(basename $(find ./src -name *.sln) '.sln')
+old_name="$1"
+new_name="$2"
+echo "Old name: $old_name"
+echo "New name: $new_name"
+
+# Derive DB name
+old_db_name=$(echo $old_name | sed -E 's/([A-Z])/_\1/g' | sed -E 's/^_//' | tr '[:upper:]' '[:lower:]')
+new_db_name=$(echo $new_name | sed -E 's/([A-Z])/_\1/g' | sed -E 's/^_//' | tr '[:upper:]' '[:lower:]')
+echo "Old DB name: $old_db_name"
+echo "New DB name: $new_db_name"
+
+# Rename directories
+find . -name *${old_name}* -type d -not -path '*/\.*' | while read old_path; do
+    new_path=$(echo $old_path | sed "s/$old_name/$new_name/")
+    if [ "$new_path" != "$old_path" ]; then
+        echo "Renaming directory $old_path to $new_path"
+        mv "$old_path" "$new_path"
+    fi
+done
+
+# Rename files
+find . -name *${old_name}* -type f -not -path '*/\.*' | while read old_path; do
+    new_path=$(echo $old_path | sed "s/$old_name/$new_name/")
+    if [ "$new_path" != "$old_path" ]; then
+        echo "Renaming file $old_path to $new_path"
+        mv "$old_path" "$new_path"
+    fi
+done
+
+# Rename code
+echo 'Renaming code...'
+grep -rl "$old_name" --include=*.{cs,csproj,sln,fsx,sh} --exclude-dir={.git,.idea,.vs,.vscode} . | while read file; do
+    sed -i -- "s/$old_name/$new_name/g" "$file"
+    rm "${file}--"
+done
+
+# Rename DB
+echo 'Renaming DB...'
+grep -rl "$old_db_name" --include=*.{sql,fsx,json} --exclude-dir={.git,.idea,.vs,.vscode} . | while read file; do
+    sed -i -- "s/$old_db_name/$new_db_name/g" "$file"
+    rm "${file}--"
+done
+
+popd # Go back to caller directory
