109c36b827885d4638cfdf7c8d9c9e992504debf
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 02:27:52 +0100

    Generate models and DAL mapping from DB schema

---
+++ src/MyApp.Core/Models/Item.cs
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// THIS CODE IS GENERATED - DO NOT CHANGE IT MANUALLY!
+////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+
+namespace MyApp.Core.Models
+{
+    public partial class Item
+    {
+        public long ItemId { get; set; }
+        public string Name { get; set; }
+        public bool IsProcessed { get; set; }
+
+        public Item()
+        {
+            
+        }
+    }
+}
---
+++ src/MyApp.Infrastructure/Data/EF/AppDb.cs
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// THIS CODE IS GENERATED - DO NOT CHANGE IT MANUALLY!
+////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Core.Models;
+
+namespace MyApp.Infrastructure.Data.EF
+{
+    public partial class AppDb : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+
+        private void ConfigureEntities(ModelBuilder modelBuilder)
+        {
+            // Item
+            var item = modelBuilder.Entity<Item>()
+                .ToTable("item", "app");
+            item.HasKey(c => c.ItemId);
+            item.Property(e => e.ItemId)
+                .HasColumnName("item_id")
+                .IsRequired();
+            item.Property(e => e.Name)
+                .HasColumnName("name")
+                .IsRequired();
+            item.Property(e => e.IsProcessed)
+                .HasColumnName("is_processed")
+                .IsRequired();
+        }
+    }
+}
