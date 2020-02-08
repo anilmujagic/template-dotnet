0f49a335d27b03df07d8b89c57cf2bce5dd0aaf7
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 02:57:58 +0100

    Create an abstract base DataService

---
+++ src/MyApp.Domain/Common/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MyApp.Core.Enums;
+using MyApp.Core.Interfaces;
+
+namespace MyApp.Domain.Common
+{
+    public abstract class DataService
+    {
+        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+        private readonly IRepositoryFactory _repositoryFactory;
+
+        protected DataService(IUnitOfWorkFactory unitOfWorkFactory, IRepositoryFactory repositoryFactory)
+        {
+            _unitOfWorkFactory = unitOfWorkFactory ?? throw new ArgumentNullException(nameof(unitOfWorkFactory));
+            _repositoryFactory = repositoryFactory ?? throw new ArgumentNullException(nameof(repositoryFactory));
+        }
+
+        protected IUnitOfWork NewUnitOfWork(UnitOfWorkMode mode = UnitOfWorkMode.ReadOnly)
+        {
+            return _unitOfWorkFactory.Create(mode);
+        }
+
+        protected IRepository<T> NewRepository<T>(IUnitOfWork unitOfWork)
+            where T : class
+        {
+            return _repositoryFactory.Create<T>(unitOfWork);
+        }
+    }
+}
