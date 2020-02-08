29fea49d09e5a003888d55537b9127c4e05455fb
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 03:54:06 +0100

    Implement API

---
+++ src/MyApp.Api/Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyApp.Core.Dtos;
+using MyApp.Core.Interfaces;
+
+namespace MyApp.Api.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IItemService _itemService;
+
+        public ItemController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get([FromQuery] string nameSearch)
+        {
+            return _itemService.GetItems(nameSearch);
+        }
+    }
+}
---
+++ src/MyApp.Api/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyApp.Common;
+using MyApp.Core;
+using MyApp.Infrastructure.DI;
+
+namespace MyApp.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            Config.SetConfigurationProvider(DependencyResolver.GetConfigurationProvider());
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
