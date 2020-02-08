e0a1ed7e3bdcb84a8195020319293807357cb6f7
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-08 03:10:07 +0100

    Add DI modules

---
+++ src/MyApp.Domain.DI/DomainModule.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Common.Extensions;
+using MyApp.Domain.Common;
+
+namespace MyApp.Domain.DI
+{
+    public class DomainModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            var domainAssembly = typeof(DataService).Assembly;
+
+            domainAssembly.GetTypes()
+                .Where(t => !t.GetTypeInfo().IsAbstract && t.Name.EndsWith("Service"))
+                .Each(implementationType =>
+                    implementationType.GetInterfaces().Each(interfaceType =>
+                        serviceCollection.AddTransient(interfaceType, implementationType)));
+        }
+    }
+}
---
+++ src/MyApp.Infrastructure.DI/InfrastructureModule.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Common.Extensions;
+using MyApp.Infrastructure.Data;
+
+namespace MyApp.Infrastructure.DI
+{
+    public class InfrastructureModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            var infrastructureAssembly = typeof(UnitOfWork).Assembly;
+
+            infrastructureAssembly.GetTypes()
+                .Where(t => !t.GetTypeInfo().IsAbstract && t.Name.EndsWith("Factory"))
+                .Each(implementationType =>
+                    implementationType.GetInterfaces().Each(interfaceType =>
+                        serviceCollection.AddTransient(interfaceType, implementationType)));
+        }
+    }
+}
