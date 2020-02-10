32ff8b8db6831406b9789d41bbc23f8c4df6c278
Anil Mujagic <anil.mujagic@gmail.com>
2020-02-10 01:05:04 +0100

    Remove unused imports

--- src/MyApp.Api/Program.cs
+++ src/MyApp.Api/Program.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using MyApp.Common;
 using MyApp.Core;
 using MyApp.Infrastructure.DI;
 
